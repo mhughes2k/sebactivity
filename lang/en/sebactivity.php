e9bfe82f2d4e80137f099106478ecc7e8f2c6edd
Michael Hughes <michaelhughes@strath.ac.uk>
2019-06-19 14:46:29 +0100

    Initial skeleton

---
+++ lang/en/sebactivity.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     sebactivity
+ * @category    string
+ * @copyright   2019 Your Name <you@example.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'sebactivity';
+$string['modulename_help'] = 'sebactivity';
+$string['modulenameplural'] = 'sebactivity';
+$string['nonewmodules'] = 'No new modules';
+$string['pluginadministration'] = 'Plugin administration';
+$string['pluginname'] = 'Module sebactivity';
+$string['sebactivity:addinstance'] = 'sebactivity add instance';
+$string['sebactivityfieldset'] = 'sebactivityfieldset';
+$string['sebactivityname'] = 'sebactivity';
+$string['sebactivityname_help'] = 'sebactivity';
+$string['sebactivitysettings'] = 'sebactivitysettings';
+$string['view'] = 'View';
